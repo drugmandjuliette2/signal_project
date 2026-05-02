48a20ffe6ddcc36de7a6f59e962a569322364ba9
drugmandjuliette2 <drugmandjuliette@gmail.com>
2026-05-02 07:56:32 +0200

    Enhance PatientDataGenerator with Javadoc comments
    Added documentation for the PatientDataGenerator interface.

--- src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
+++ src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
@@ -2,6 +2,19 @@ package com.cardio_generator.generators;
 
 import com.cardio_generator.outputs.OutputStrategy;
 
+/**
+ * This is the blueprint for all data generators in the simulator.
+ * * Any class that implements this interface is promising to provide a way 
+ * to create specific health data for a patient and send it to an output.
+ */
 public interface PatientDataGenerator {
+
+    /**
+     * This method must be implemented by any generator to define how it
+     * creates data and where that data goes.
+     *
+     * @param patientId The ID of the patient we are currently simulating.
+     * @param outputStrategy The method used to handle the resulting data (e.g., printing or saving).
+     */
     void generate(int patientId, OutputStrategy outputStrategy);
 }
